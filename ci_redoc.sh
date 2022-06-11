09adcad103161e2f4961bdb2ffe61d3402f09e1c
Igor Kim <igor.skh@gmail.com>
2022-06-11 11:31:08 +0200

    update build script

--- ci_redoc.sh
+++ ci_redoc.sh
@@ -1,11 +1,5 @@
 rm -rf public
 mkdir -p public
-cp public_template.html public/index.html
 
-FILES="complete-swagger-*.yaml"
-for f in $FILES
-do
-    echo "Processing $f file..."
-    npx redoc-cli bundle $f
-    mv redoc-static.html public/$f.html
-done
+npx redoc-cli bundle complete-swagger-main.yaml
+mv redoc-static.html public/index.html
